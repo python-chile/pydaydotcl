87d5b2ed9c3766a56412f797ce91d427ee9f80b2
MariferVL <mfvl.1186@gmail.com>
2025-04-23 21:47:51 -0400

    Add config files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  transpilePackages: ["framer-motion"],
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
