52e91340418aff3443d85762abec991c83d03754
MariferVL <mfvl.1186@gmail.com>
2025-05-02 19:06:44 -0400

    Test without next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -19,27 +19,52 @@ const nextConfig = {
     minimumCacheTTL: 86400,
     unoptimized: false,
   },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
-          },
-          {
-            key: 'X-Frame-Options',
-            value: 'DENY',
-          },
-          {
-            key: 'X-XSS-Protection',
-            value: '1; mode=block',
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   transpilePackages: ["framer-motion"],
+//   trailingSlash: true,
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "**",
+//       },
+//       {
+//         protocol: "http",
+//         hostname: "localhost",
+//         port: "3000",
+//       },
+//     ],
+//     formats: ["image/webp"],
+//     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+//     minimumCacheTTL: 86400,
+//     unoptimized: false,
+//   },
+//   async headers() {
+//     return [
+//       {
+//         source: '/(.*)',
+//         headers: [
+//           {
+//             key: 'X-Content-Type-Options',
+//             value: 'nosniff',
+//           },
+//           {
+//             key: 'X-Frame-Options',
+//             value: 'DENY',
+//           },
+//           {
+//             key: 'X-XSS-Protection',
+//             value: '1; mode=block',
+//           },
+//         ],
+//       },
+//     ];
+//   },
+// };
+
+// export default nextConfig;
--- src/middleware.js
+++ src/middleware.js
@@ -1,29 +1,29 @@
-// import { NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
-// export function middleware(request) {
-//   const userAgent = request.headers.get("user-agent") || "";
-//   const isBot = /bot|facebookexternalhit|twitterbot|linkedinbot/.test(
-//     userAgent.toLowerCase()
-//   );
-//   const path = request.nextUrl.pathname;
-//   const searchParams = new URLSearchParams(request.nextUrl.searchParams);
+export function middleware(request) {
+  const userAgent = request.headers.get("user-agent") || "";
+  const isBot = /bot|facebookexternalhit|twitterbot|linkedinbot/.test(
+    userAgent.toLowerCase()
+  );
+  const path = request.nextUrl.pathname;
+  const searchParams = new URLSearchParams(request.nextUrl.searchParams);
 
-//   const blockedPaths = {
-//     "/register": process.env.NEXT_PUBLIC_FEATURE_REGISTRATION !== "true",
-//     "/sponsors": process.env.NEXT_PUBLIC_FEATURE_SPONSORS !== "true",
-//   };
+  const blockedPaths = {
+    "/register": process.env.NEXT_PUBLIC_FEATURE_REGISTRATION !== "true",
+    "/sponsors": process.env.NEXT_PUBLIC_FEATURE_SPONSORS !== "true",
+  };
 
-//   if (isBot) return NextResponse.next();
+  if (isBot) return NextResponse.next();
 
-//   for (const [route, isBlocked] of Object.entries(blockedPaths)) {
-//     if (path.startsWith(route) && isBlocked) {
-//       searchParams.set(
-//         "maintenance",
-//         encodeURIComponent(`Acceso temporalmente deshabilitado: ${route}`)
-//       );
-//       return NextResponse.redirect(new URL(`/?${searchParams}`, request.url));
-//     }
-//   }
+  for (const [route, isBlocked] of Object.entries(blockedPaths)) {
+    if (path.startsWith(route) && isBlocked) {
+      searchParams.set(
+        "maintenance",
+        encodeURIComponent(`Acceso temporalmente deshabilitado: ${route}`)
+      );
+      return NextResponse.redirect(new URL(`/?${searchParams}`, request.url));
+    }
+  }
 
-//   return NextResponse.next();
-// }
+  return NextResponse.next();
+}
