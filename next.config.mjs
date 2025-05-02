8f8271ef01b1ba394f3cbafc85bf09e074fa3416
MariferVL <mfvl.1186@gmail.com>
2025-05-02 19:36:22 -0400

    Test las week config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["framer-motion"],
+  // output: "export",
   trailingSlash: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "3000",
-      },
-    ],
-    formats: ["image/webp"],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    minimumCacheTTL: 86400,
-    unoptimized: false,
+    unoptimized: true, 
+    remotePatterns: []
   },
+  trailingSlash: true,
+  skipTrailingSlashRedirect: true
 };
 
 export default nextConfig;
-
 // /** @type {import('next').NextConfig} */
 // const nextConfig = {
 //   transpilePackages: ["framer-motion"],
