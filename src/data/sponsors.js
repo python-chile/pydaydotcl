d3ebffa758295da16676c1b32e8e6da3f2105e2a
LiliGC <liliana.garmendia.c@gmail.com>
2025-05-02 23:39:05 -0400

    update sponsors data

--- src/data/sponsors.js
+++ src/data/sponsors.js
@@ -1,16 +1,15 @@
 // Ejemplo sponsors 2024:
-// {
-//   _id: 1,
-//   logo: { asset: { url: "/images/sponsors/psf.webp" } },
-//   name: "Python Software Foundation",
-// }
-//{
-//  _id: 2,
-//  logo: { asset: { url: "/images/sponsors/aws.webp" } },
-//  name: "Amazon Web Services",
-//},
-
 const sponsors = [
+  {
+    _id: 1,
+    logo: { asset: { url: "/images/sponsors/psf.webp" } },
+     name: "Python Software Foundation",
+   },
+  {
+   _id: 2,
+  logo: { asset: { url: "/images/sponsors/aws.webp" } },
+  name: "Amazon Web Services",
+  },
 ];
 
 export default sponsors;
