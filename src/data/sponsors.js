39bc6478d8bd36d54ab10cfaeaf8e673f7f10a04
MariferVL <mfvl.1186@gmail.com>
2025-05-02 23:44:18 -0400

    Reestablish sponsors data

--- src/data/sponsors.js
+++ src/data/sponsors.js
@@ -11,6 +11,16 @@
 //},
 
 const sponsors = [
+  {
+    _id: 1,
+    logo: { asset: { url: "/images/sponsors/psf.webp" } },
+    name: "Python Software Foundation",
+  },
+  {
+    _id: 2,
+    logo: { asset: { url: "/images/sponsors/aws.webp" } },
+    name: "Amazon Web Services",
+  },
 ];
 
 export default sponsors;
